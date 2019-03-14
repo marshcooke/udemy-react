823f14c7089c92e9eb09941bb12598461a8644e4
Sarah Cooke <marshcooke1@gmail.com>
2019-03-13 23:55:12 -0500

    Babel compiled code from src/app.js to public/scripts/app.js

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('App.js is Running');
+
+// JSX = JavaScript XML
+var template = React.createElement(
+  'p',
+  null,
+  'This is JSX from app.js.'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
