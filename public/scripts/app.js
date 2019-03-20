3f59bf58838fb35af3ffd9ee468f0345c30c3b7c
Sarah Cooke <marshcooke1@gmail.com>
2019-03-19 22:24:58 -0500

    Update and challenge section within playground file on arrow function instruction

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,40 @@
-"use strict";
+'use strict';
 
-function square(x) {
-    return x * x;
+// function square(x) {
+//     return x * x;
+// };
+
+// console.log(square(3));
+
+// // const squareArrow = (x) => {
+// //     return x * x;
+// // }
+
+// const squareArrow = (x) => x * x; 
+
+// console.log(squareArrow(4));
+
+// use arrow functions
+// getFirstName
+// using this function as reference:
+// if (fullName) {
+//     firstName = fullName.split(' ')[0];
+//     console.log(firstName);
+// };
+
+// console.log(firstName);
+// getFirstName('Mike Smith') -> "Mike"
+// create regular arrow function
+// create arrow function using shorthand syntax
+
+var getFirstName1 = function getFirstName1(fullName) {
+    return fullName.split(' ')[0];
 };
 
-console.log(square(3));
+console.log(getFirstName1('Sarah Cooke'));
 
-var squareArrow = function squareArrow(x) {
-    return x * x;
+var getFirstName = function getFirstName(fullerName) {
+    return fullerName.split(' ')[0];
 };
 
-console.log(squareArrow(9));
+console.log(getFirstName('Sarah Cooke'));
--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,11 +1,36 @@
-function square(x) {
-    return x * x;
+// function square(x) {
+//     return x * x;
+// };
+
+// console.log(square(3));
+
+// // const squareArrow = (x) => {
+// //     return x * x;
+// // }
+
+// const squareArrow = (x) => x * x; 
+
+// console.log(squareArrow(4));
+
+// use arrow functions
+// getFirstName
+    // using this function as reference:
+    // if (fullName) {
+    //     firstName = fullName.split(' ')[0];
+    //     console.log(firstName);
+    // };
+
+    // console.log(firstName);
+// getFirstName('Mike Smith') -> "Mike"
+// create regular arrow function
+// create arrow function using shorthand syntax
+
+const getFirstName1 = (fullName) => {
+    return fullName.split(' ')[0];
 };
 
-console.log(square(3));
+console.log(getFirstName1('Sarah Cooke'));
 
-const squareArrow = (x) => {
-    return x * x;
-}
+const getFirstName = (fullerName) => fullerName.split(' ')[0];
 
-console.log(squareArrow(9));
+console.log(getFirstName('Sarah Cooke'));
