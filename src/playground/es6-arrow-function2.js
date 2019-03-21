47d539b39fbbca777d0181b79241e1c76fd58127
Sarah Cooke <marshcooke1@gmail.com>
2019-03-20 22:27:23 -0500

    Stage two of arrow function instruction within playground directory

--- src/playground/es6-arrow-function2.js
+++ src/playground/es6-arrow-function2.js
@@ -1,24 +1,18 @@
-'use strict';
-
 // arguments object - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
     // console.log(arguments);
     return a + b;
 };
-console.log(add(55, 1, 10001));
+console.log(add(55,1, 10001));
 
 // this keyword - no longer bound with arrow functions
 
-var user = {
+const user = {
     name: 'Sarah',
     cities: ['Chesapeake', 'Aiken', 'Fond du Lac', 'Stevens Point', 'London', 'Minneapolis'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city + '!';
-        });
+    printPlacesLived() {
+        return this.cities.map((city) => this.name + ' has lived in ' + city + '!');
     }
 };
 console.log(user.printPlacesLived());
