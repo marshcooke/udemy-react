799e1ca20c4d7c23ff67324ff9ff7849ebec73e1
Sarah Cooke <marshcooke1@gmail.com>
2019-03-26 21:05:42 -0500

    Add logic behind and button for onMakeDecision

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,6 +26,12 @@ var onRemoveAll = function onRemoveAll() {
     render();
 };
 
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+};
+
 var appRoot = document.getElementById('app');
 
 var render = function render() {
@@ -48,9 +54,9 @@ var render = function render() {
             app.options.length > 0 ? 'Here are your options:' : 'There are no options'
         ),
         React.createElement(
-            'p',
-            null,
-            app.options.length
+            'button',
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What Should I do?'
         ),
         React.createElement(
             'button',
