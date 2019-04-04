35f95f3f51c2e4b1b4d904acf17aa907bfe1b649
Sarah Cooke <marshcooke1@gmail.com>
2019-04-03 20:48:21 -0500

    Add additional components from the challenge.

--- src/app.js
+++ src/app.js
@@ -1,29 +1,54 @@
 class Header extends React.Component {
     render() {
-        return {
+        return (
             <div>
                 <h1>Indecision</h1>
                 <h2>Put your life in the hands of a computer</h2>
             </div>
-        };
+        );
     }
 }
 
 class Action extends React.Component {
     render() {
-        return {
+        return (
             <div>
                 <button>What should I do?</button>
             </div>
-        };
+        );
     }
 }
 
-const jsx = {
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Options component here</p>
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>AddOptions component here</p>
+            </div>
+        )
+    }
+}
+// Options -> render static text: Options component here
+
+// AddOption -> AddOptions component here 
+
+const jsx = (
     <div>
         <Header />
         <Action />
+        <Options />
+        <AddOption />
     </div>
-};
+);
 
 ReactDOM.render(jsx, document.getElementById('app'));
