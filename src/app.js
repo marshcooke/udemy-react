ba40ae3098c8448c3130ff159f9b46ede111da59
Sarah Cooke <marshcooke1@gmail.com>
2019-04-06 11:33:12 -0500

    Set up binding for handleRemoveAll in Options

--- src/app.js
+++ src/app.js
@@ -1,14 +1,3 @@
-const obj = {
-    name: 'Vikram',
-    getName() {
-        return this.name;
-    }
-};
-
-const getName = obj.getName.bind(obj);
-
-console.log(getName());
-
 class IndecisionApp extends React.Component {
     render() {
         const title = 'Indecision';
@@ -51,6 +40,10 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
     handleRemoveAll() {
         console.log(this.props.options);
         // alert('handleRemoveAll');
