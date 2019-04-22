f14d3bd65de1fc92a5b7149d66de65188a6ae3dd
Sarah Cooke <marshcooke1@gmail.com>
2019-04-22 00:05:14 -0500

    Saving and loading the count based on state locally

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -8,6 +8,22 @@ class Counter extends React.Component {
             count: 0
         };
     }
+    componentDidMount() {
+        const stringCount = localStorage.getItem('count');
+        const count = parseInt(stringCount, 10);
+
+        if (!isNaN(count)) {
+            this.setState(() => ({ count}));
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count);
+        }
+    }
+    // componentWillUnmount() {
+    //     console.log('componentWillUnmount');
+    // }
     handleAddOne() {
         this.setState((prevState) => {
             return {
