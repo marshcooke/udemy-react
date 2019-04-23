e99fe83f4982520643da5bcb0e1ca0731e8a00da
Sarah Cooke <marshcooke1@gmail.com>
2019-04-22 23:59:36 -0500

    Include path to output with path.join

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 // entry point -> output 
 
+const path = require('path');
+
 module.exports = {
-    entry: './src/app.js'
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
 };
