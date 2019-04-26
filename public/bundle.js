fa123c7d1413e23017dbbb2c857a20ca6836a165
Sarah Cooke <marshcooke1@gmail.com>
2019-04-25 23:18:26 -0500

    Pre-challenge notes in app.js

--- public/bundle.js
+++ public/bundle.js
@@ -65,37 +65,22 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(1);
 // import './utils.js';
+// import { square, add } from './utils.js';
 
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(100, 23));
 
-console.log('app.js is running!');
-console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["b" /* square */](4));
-console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* add */](100, 23));
+// create new person.js file
+// which will export two named exports 
+// isAdult(18) - true if adult, otherwise false
+// canDrink(18) - true if 21 and over, otherwise false
 
-
-/***/ }),
-/* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-console.log('utils.js is running.');
-
-const square = (x) => x * x;
-/* harmony export (immutable) */ __webpack_exports__["b"] = square;
-
-
-const add = (a, b) => a + b;
-/* harmony export (immutable) */ __webpack_exports__["a"] = add;
-
-
-// export { square, add }; -- this is no longer needed because it's handled above ahead of the const.
-
-// exports - default export - named exports
+// import isAdult and canDrink
+// use both priniting result to the console 
 
 
 /***/ })
--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 // import './utils.js';
-import { square, add } from './utils.js';
+// import { square, add } from './utils.js';
 
-console.log('app.js is running!');
-console.log(square(4));
-console.log(add(100, 23));
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+// create new person.js file
+// which will export two named exports 
+// isAdult(18) - true if adult, otherwise false
+// canDrink(18) - true if 21 and over, otherwise false
+
+// import isAdult and canDrink
+// use both priniting result to the console 
