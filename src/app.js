49b7ba6d50b08bf694f38db4a4879416846f2cfe
Sarah Cooke <marshcooke1@gmail.com>
2019-04-27 17:49:42 -0500

    Addition of react and react-dom in node_modules and inital set up for importing and using both in app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 // install -> import -> use
-import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log(validator.isEmail('test@gmail.com'));
+const template = React.createElement ('p', {}, 'testing 123');
+
+ReactDOM.render(template, document.getElementById('app'));
