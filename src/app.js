19e3bac64c7f263978ef329202631cdc9be0cd01
Sarah Cooke <marshcooke1@gmail.com>
2019-04-29 19:49:00 -0500

    Exporting AddOption.js into app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import AddOption from './components/AddOption';
+
 class IndecisionApp extends React.Component {
     constructor(props) {
         super(props);
@@ -138,38 +140,5 @@ const Option = (props) => {
         </div>
     );
 };
-
-class AddOption extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            error: undefined
-        };
-    }
-    handleAddOption(e) {
-        e.preventDefault();
-
-        const option = e.target.elements.option.value.trim();
-        const error = this.props.handleAddOption(option);
-
-        this.setState(() => ({ error }));
-
-        if (!error) {
-            e.target.elements.option.value = '';
-        }
-    }
-    render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.handleAddOption}>
-                    <input type="text" name="option" />
-                    <button>Add Option</button>
-                </form>
-            </div>
-        );
-    }
-}
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
