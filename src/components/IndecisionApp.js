9110b563f189b9414f1f09beadabd1d0845e6459
Sarah Cooke <marshcooke1@gmail.com>
2019-05-01 20:24:15 -0500

    Syntax and spacing update as well as removal of challenge comments

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -4,39 +4,34 @@ import Header from './Header';
 import Action from './Action';
 import Options from './Options';
 
-// pull the state out of constructor
-// convert all 4 event handlers to class properties (arrow functions)
-// delete constructor completely
-// start with class properties and end with methods
-    // event handlers should be at the top, under state
-
 export default class IndecisionApp extends React.Component {
     state = {
         options: []
     };
     handleDeleteOptions = () => {
         this.setState(() => ({ options: [] }));
-    }
+    };
     handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({
             options: prevState.options.filter((option) => optionToRemove !== option)
         }));
-    } 
+    };
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
         alert(option);
-    }
+    };
     handleAddOption = (option) => {
         if (!option) {
             return 'Enter valid value to add an option.';
         } else if (this.state.options.indexOf(option) > -1) {
             return 'This option already exists.';
         }
+        
         this.setState((prevState) => ({ 
             options: prevState.options.concat(option)
         }));
-    }
+    };
     componentDidMount() {
         try {
             const json = localStorage.getItem('options');
