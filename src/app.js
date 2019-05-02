e88948447dc90cbc1bc760452cca3b7f7653bbdd
Sarah Cooke <marshcooke1@gmail.com>
2019-05-01 20:07:57 -0500

    Amend class properties in app.js and AddOption.js

--- src/app.js
+++ src/app.js
@@ -3,3 +3,19 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+    constructor() {
+        this.name = 'Sarah';
+    }
+}
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+// ------
+
+class NewSyntax {
+    name = 'Jen';
+}
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
