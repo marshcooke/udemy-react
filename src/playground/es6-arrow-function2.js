f4b0424b4d4d5398a972a2466cdd562165b7b014
Sarah Cooke <marshcooke1@gmail.com>
2019-06-07 06:50:19 -0500

    Adjust spacing and formatting

--- src/playground/es6-arrow-function2.js
+++ src/playground/es6-arrow-function2.js
@@ -1,5 +1,4 @@
 // arguments object - no longer bound with arrow functions
-
 const add = (a, b) => {
     // console.log(arguments);
     return a + b;
@@ -7,7 +6,6 @@ const add = (a, b) => {
 console.log(add(55,1, 10001));
 
 // this keyword - no longer bound with arrow functions
-
 const user = {
     name: 'Sarah',
     cities: ['Chesapeake', 'Aiken', 'Fond du Lac', 'Stevens Point', 'London', 'Minneapolis'],
@@ -18,7 +16,6 @@ const user = {
 console.log(user.printPlacesLived());
 
 // Challenge area
-
 const multiplier = {
     numbers: [10, 20, 30],
     multiplyBy: 3,
