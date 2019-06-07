5c85b09071d695270b8701881f2c1a68eb5a6dba
Sarah Cooke <marshcooke1@gmail.com>
2019-06-07 06:49:47 -0500

    Removed old, commented out code from es6-arrow-function

--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,30 +1,3 @@
-// function square(x) {
-//     return x * x;
-// };
-
-// console.log(square(3));
-
-// // const squareArrow = (x) => {
-// //     return x * x;
-// // }
-
-// const squareArrow = (x) => x * x; 
-
-// console.log(squareArrow(4));
-
-// use arrow functions
-// getFirstName
-    // using this function as reference:
-    // if (fullName) {
-    //     firstName = fullName.split(' ')[0];
-    //     console.log(firstName);
-    // };
-
-    // console.log(firstName);
-// getFirstName('Mike Smith') -> "Mike"
-// create regular arrow function
-// create arrow function using shorthand syntax
-
 const getFirstName1 = (fullName) => {
     return fullName.split(' ')[0];
 };
